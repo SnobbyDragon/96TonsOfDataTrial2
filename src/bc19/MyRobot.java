e41221c48528f6670a6c97e520deed611f5e3c6d
tdiblasi <32341607+tdiblasi@users.noreply.github.com>
2019-01-13 21:10:36 -0500

    Update MyRobot.java

--- src/bc19/MyRobot.java
+++ src/bc19/MyRobot.java
@@ -29,8 +29,59 @@ public class MyRobot extends BCAbstractRobot {
 				//log(Integer.toString([0][getVisibleRobots()[0].castle_talk]));
 			}
 		}
+		
+		//sends signal max of 10 sqaure radius if damage was just taken
+    		alertEnemiesNearby();
+		
+		//mines if mining is possible
+    		//May want to change to only mine if safe
+    		if (canMineFuel()) {
+    			return mine();
+    		}
+    		if(canMineKarbonite()) {
+    			return mine();
+    		}
+    	
+    		lastHealth = me.health;
 
 		return null;
 
 	}
+	
+	//if damage was taken, sends signal
+    	//696969 is alert value :)
+    	//broadcasts sq radius of 10 or, if less than 10 fuel, longest possible distance
+    	public void alertEnemiesNearby() {
+    		if(me.health>me.lastHealth) {
+    			log("i took damage");
+    			int sigRad = 10;
+    			if(sigRad>me.fuel) {
+    				sigRad=me.fuel;
+    			}
+    			log("sending alert");
+    			signal(696969,sigRad);
+    		}
+    	}
+	
+	//returns true if pilgrim can mine fuel
+    	public boolean canMineFuel() {
+    		if (me.unit == SPECS.PILGRIM && (fuelMap[me.y][me.x]) {
+    			if (me.fuel>0&&me.fuel<100){
+    				log("mining fuel");
+    				return true;
+    			}
+    		}
+   	 }
+		    
+	//returns true if pilgrim can mine karbonite
+    	public boolean canMineKarbonite() {
+    	if me.unit == SPECS.PILGRIM && (karboniteMap[me.y][me.x]) {
+			if(me.fuel>0&&me.karbonite<20) {
+				log("mining karbonite");
+				return true;
+			}
+    	}
+    	}
+		    
+	
 }
